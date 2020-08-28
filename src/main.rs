d7d067227648f46a329557a150701788b3ab2009
iRaiko <jlardenoije@gmail.com>
2020-08-28 14:21:37 +0200

    Added different cbt files, implemented json

--- src/main.rs
+++ src/main.rs
@@ -3,33 +3,36 @@ extern crate clap;
 use clap::{Arg, App};
 use std::fs;
 
-fn main() {
+mod cbt_json;
+
+fn main(){
     let matches = App::new("poly")
-        .author(crate_authors!())
-        .version(crate_version!())
-        .arg(Arg::with_name("input")
-            .help("The file from which to convert from")
-            .index(1)
-            .required(true)
-            .validator(file_validator))
-        .arg(Arg::with_name("output")
-            .help("The file location of the converted input file")
-            .index(2)
-            .required(true))
-        .arg(Arg::with_name("from")
-            .help("What format the input file is converted from")
-            .required(true)
-            .possible_values(&["yaml", "toml", "json", "ini"]))
-        .arg(Arg::with_name("to")
-            .help("What format the input file is converted too")
-            .required(true)
-            .possible_values(&["yaml", "toml", "json", "ini"]))
-        .arg(Arg::with_name("append")
-            .help("Append to end of file instead of overwriting")
-            .short("a")
-            .long("append"))
-        .get_matches();
+    .author(crate_authors!())
+    .version(crate_version!())
+    .arg(Arg::with_name("input")
+        .help("The file from which to convert from")
+        .index(1)
+        .required(true)
+        .validator(file_validator))
+    .arg(Arg::with_name("output")
+        .help("The file location of the converted input file")
+        .index(2)
+        .required(true))
+    .arg(Arg::with_name("from")
+        .help("What format the input file is converted from")
+        .required(true)
+        .possible_values(&["yaml", "toml", "json", "ini"]))
+    .arg(Arg::with_name("to")
+        .help("What format the input file is converted too")
+        .required(true)
+        .possible_values(&["yaml", "toml", "json", "ini"]))
+    .arg(Arg::with_name("append")
+        .help("Append to end of file instead of overwriting")
+        .short("a")
+        .long("append"))
+    .get_matches();
 }
+
 
 fn file_validator(v: String) -> Result<(), String>{
     let metadata = fs::metadata(v);
