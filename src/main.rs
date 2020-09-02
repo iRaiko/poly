f3e1d10b65bbdb760be52bb11ba35d73c5edd8c1
iRaiko <jlardenoije@gmail.com>
2020-09-02 15:13:48 +0200

    Updated help message

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 #[macro_use]
 extern crate clap;
-use clap::{Arg, App, SubCommand};
+use clap::{Arg, App, SubCommand, AppSettings};
 use std::fs;
 
 
@@ -10,31 +10,63 @@ mod cbt_yaml;
 mod cbt_toml;
 
 fn main(){
-    let arg_vec = vec!["poly", "morph", "json", "test.txt", "to", "yaml", "bla.txt"];
+    let _arg_vec = vec!["poly", "morph", "json", "test.txt", "to", "yaml", "bla.txt"];
 
     let possible_formats = vec!["json", "toml", "yaml", "ini"];
 
     let matches = App::new("poly")
         .author(crate_authors!())
         .version(crate_version!())
+        .setting(AppSettings::SubcommandRequired)
+        .help(
+"
+Polymorh
+
+Poly v1
+
+Usage:
+    poly.exe morph <from_format> <from_file> to <to_format> <to_file>
+Example: 
+    poly.exe morph json test.txt to yaml converted.txt
+
+Subcommands:
+    morph
+    to
+
+Arguments:
+    <from_format>   Set the format from which to convert the file from with possible values:
+                    [json, toml, yaml, ini]
+    <from_file>     Set the file from which to convert from.
+                    Example: C:\\Users\\Harry Potter\\example.txt
+    <to_format>     Set the format from which to convert the file to with possible values:
+                    [json, toml, yaml, ini]
+    <to_file>       The location for the file the conversion will be saved to.
+                    Example: C:\\Users\\Harry Potter\\converted_example.txt
+
+"
+    )
+        .usage("poly.exe morph <from_format> <from_file> to <to_format> <to_file>\n    Example: poly.exe morph json test.txt to yaml converted.txt")
         .subcommand(SubCommand::with_name("morph")
+            .setting(AppSettings::SubcommandRequired)
+            .usage("poly.exe morph <from_format> <from_file> to <to_format> <to_file>")
             .arg(Arg::with_name("from_type")
-                .help("")
+                .help("Set the format from which to convert the file from.\nPossible values:")
                 .required(true)
                 .possible_values(&possible_formats))
             .arg(Arg::with_name("from_file")
-                .help("")
+                .help("Set the file from which to convert from.\nExample: C:\\Users\\Harry Potter\\example.txt")
                 .required(true))
             .subcommand(SubCommand::with_name("to")
-                    .arg(Arg::with_name("to_type")
-                        .help("")
-                        .required(true)
-                        .possible_values(&possible_formats))
-                    .arg(Arg::with_name("to_file")
-                        .help("")
-                        .required(true))))
-        //.get_matches();
-        .get_matches_from(arg_vec);
+                .usage("poly.exe morph <from_format> <from_file> to <to_format> <to_file>")
+                .arg(Arg::with_name("to_type")
+                    .help("Set the format from which to convert the file to.\nPossible values:")
+                    .required(true)
+                    .possible_values(&possible_formats))
+                .arg(Arg::with_name("to_file")
+                    .help("The location for the file the conversion will be saved to.\nExample: C:\\Users\\Harry Potter\\converted_example.txt")
+                    .required(true))))
+        .get_matches();
+        //.get_matches_from(arg_vec);
 
 
 
@@ -56,6 +88,8 @@ fn main(){
         println!("The file name is {}", path);
         let content = load_file(path);
 
+        println!("{}", content);
+
         let deserialized_content = match format{
             "json" => cbt_json::deserialize_json(content),
             "yaml" => cbt_yaml::deserialize_yaml(content),
@@ -68,11 +102,12 @@ fn main(){
 
         if let Some(submatches) = submatches.subcommand_matches("to")
         {
-            println!("The format we are converting to is {}", submatches.value_of("to_type").expect("No value"));
-            let path = submatches.value_of("to_file").expect("No value");
-            println!("The file we are saving to is {}", path);
+            let new_format = submatches.value_of("to_type").expect("No value");
+            println!("The format we are converting to is {}", new_format);
+            let new_path = submatches.value_of("to_file").expect("No value");
+            println!("The file we are saving to is {}", new_path);
 
-            let serialized_content = match format{
+            let serialized_content = match new_format{
                 "json" => cbt_json::serialize_json(deserialized_content),
                 "yaml" => cbt_yaml::serialize_yaml(deserialized_content),
                 "toml" => cbt_toml::serialize_toml(deserialized_content),
@@ -82,7 +117,7 @@ fn main(){
 
             println!("{}", serialized_content);
 
-            let result = save_file(path, serialized_content);
+            let result = save_file(new_path, serialized_content);
             println!("{}", result);
         }
     }
@@ -100,7 +135,7 @@ fn save_file(path: &str, content: String) -> String{
     String::from("Saved file")
 }
 
-fn file_validator(v: String) -> Result<(), String>{
+fn _file_validator(v: String) -> Result<(), String>{
     let metadata = fs::metadata(v);
     match metadata {
         Ok(file) =>  if file.is_file(){ return Ok(())} else { Err(String::from("The value is not a valid file")) } 
